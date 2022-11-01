ea35917492999a0464969b427513e0d49939b203
mousesaver <sean.fan87@gmail.com>
2022-11-01 11:13:47 -0700

    deployment

--- backend/settings.py
+++ backend/settings.py
@@ -146,4 +146,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [ 'http://localhost:3000' ]
+CORS_ORIGIN_WHITELIST = [ 'http://localhost:3000', 'https://splendid-donut-46f7ba.netlify.app' ]
